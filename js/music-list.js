66a5c9903e9cce77f7762d7ad0ce2e6bbee8505e
tài code <109515588+deeth781@users.noreply.github.com>
2023-01-07 23:13:27 +0700

    Update music-list.js
    Signed-off-by: tài code <109515588+deeth781@users.noreply.github.com>

--- js/music-list.js
+++ js/music-list.js
@@ -1,66 +1,66 @@
 let allMusic = [{
         name: "Trời Giấu Trời Mang Đi",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "music-1",
         src: "music-1"
     },
     {
         name: "Giấc Mơ Rất Thơ",
-        artist: "MLưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "music-2",
         src: "music-2"
     },
     {
         name: "Tình Thật Tình Mơ",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "music-3",
         src: "music-3"
     },
     {
         name: "Đắp Mộ Magaming",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "2",
         src: "2"
     },
     {
         name: "Thương Cho Tấm Thân",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "3",
         src: "3"
     },
     {
         name: "Thà Yêu Lấy Một Người Bình Thường",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "4",
         src: "4"
     },
     {
         name: "Ngàn Câu Hứa",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "5",
         src: "5"
     },
     {
         name: "Nắm Tay Nhau",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "6",
         src: "6"
     },
     {
         name: "Sợ Ngày Mai Em Đi Mất",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "7",
         src: "7"
     },
     {
         name: "Trót Trao Duyên",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "8",
         src: "8"
     },
     {
         name: "Ngày Thơ ẤU",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Vũ Anh Tài",
         img: "9",
         src: "9"
     },
